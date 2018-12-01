1c7d6f9f1c5bab42e1d73dfa53c1f9d13284e7a9
Srinivas Dhanwada <dhanwada.dev@gmail.com>
2018-12-01 15:45:40 -0600

    Add Debug Output Flag
    This commit adds a debug output flag that prints debug messages. Debug messages are added to the main loop.

--- parse_xml.sh
+++ parse_xml.sh
@@ -6,6 +6,10 @@
 . ./list.sh
 . ./stack.sh
 
+echod() {
+  ! [ -z "$_OPT_DEBUG" ] && echo $@ >&2
+}
+
 print_map() {
   [ "$#" -lt 1 ] || [ "$#" -gt 2 ] && return 1
   local spacer=""
@@ -48,21 +52,26 @@ parse_xml() {
 
   # Create empty map to store parsed results
   CURR_TAG_MAP="$(util_make_ref "parsed")"
+  echod "CURR_TAG_MAP initialized to: $CURR_TAG_MAP"
 
   # Start Parsing
   while read -r LINE; do
+    echod "Starting parse for: $LINE"
     # Check if we have an empty line
     if [ "x${LINE}x" = "xx" ]; then
+      echod "Empty line, continuing..."
       continue
     fi
 
     # Check for comments in the form <!-- * --> or <? * ?>
     if [ "$(echo "<$LINE" | grep -E "^(<\?|<!--)")" != "" ]; then
       ISCOMMENT="1"
+      echod "Comment Start Found! Skipping until comment end is found..."
     fi
     if [ "$ISCOMMENT" = "1" ]; then
       if [ "$(echo "<$LINE" | grep -E "(\?>|-->)$")" != "" ]; then
         ISCOMMENT="0"
+        echod "Comment End Found! Resuming normal parsing..."
       fi
       continue
     fi
@@ -82,6 +91,8 @@ EOF
     # discard attributes if they exist
     local TAG="$(echo "$ENTITY" | cut -f1 -d" ")"
 
+    echod "Parts Determined: tag: |$TAG| value: |$VALUE| entity: |$ENTITY|"
+
     # Check if the tag closes our current tag
     if [ "x${TAG}x" = "x/${CURR_TAG_NAME}x" ]; then
       CURR_TAG_NAME=""
@@ -89,33 +100,42 @@ EOF
         CURR_TAG_NAME="$(stack_pop "$TAG_NAME_STACK")"
       fi
 
+      echod "Closing tag determined, current tag updated to: $CURR_TAG_NAME"
+
       if [ "$(stack_size "$TAG_MAP_STACK")" != 0 ]; then
         local prev_map="$(stack_pop "$TAG_MAP_STACK")"
         list_push "$prev_map" "$CURR_TAG_MAP" > /dev/null
         CURR_TAG_MAP="$prev_map"
+        echod "Tag stack added, current tag map updated to $CURR_TAG_MAP"
       fi
       continue
     fi
 
     # Check if this is a closing tag -- if so, we have an invalid format
     if [ "x$(echo "$TAG" | cut -c 1-1)x" = "x/x" ]; then
+      echod "Invalid Format determined! Non-matching tag found: $TAG"
       return 1
     fi
 
     # Tag is not a closing tag, It must be a nested tag
+    echod "tag: $TAG determined as nested tag"
     stack_push "$TAG_NAME_STACK" "$CURR_TAG_NAME" > /dev/null
     CURR_TAG_NAME="$TAG"
     stack_push "$TAG_MAP_STACK" "$CURR_TAG_MAP" > /dev/null
     CURR_TAG_MAP="$(util_make_ref "$TAG")"
     map_put "$CURR_TAG_MAP" "tag" "$TAG" > /dev/null
 
+    echod "Current tag updated: |$TAG|"
+    echod "Current tag map updated: |$CURR_TAG_MAP|"
+
     if [ "x${VALUE}x" != "xx" ]; then
+      echod "Pushing value onto current tag map: $VALUE"
       map_put "$CURR_TAG_MAP" "value" "$VALUE" > /dev/null
     fi
   done
   # Check for invalid structure
   if [ "$(stack_size "$TAG_NAME_STACK")" != "0" ]; then
-    echo "invalid structure!"
+    echod "Invalid structure determined!"
     return 1
   fi
 
@@ -126,14 +146,17 @@ EOF
 
 _OPT_PRINT=""
 _OPT_HELP=""
+_OPT_DEBUG=""
 
 USAGE="${0} [-p] <file.xml>
-  -p - Print summary"
+  -p - Print summary
+  -d - Print debug output"
 
-while getopts :ph _OPT 2>/dev/null; do
+while getopts :phd _OPT 2>/dev/null; do
   case ${_OPT} in
     p) _OPT_PRINT="1" ;;
     h) _OPT_HELP="1" ;;
+    d) _OPT_DEBUG="1" ;;
     *) _NOARGS="${_NOARGS}${_NOARGS+, }-${OPTARG}" ;;
   esac
 done
@@ -152,6 +175,7 @@ FILE="$1"
 # This usage of sed is more portable...
 FILE_CONTENT="$(sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' "$FILE" | tr '<' '\n')"
 
+echod "$FILE_CONTENT"
 
 PARSED="$(echo "${FILE_CONTENT}" | parse_xml)"
 if [ "$?" -ne 0 ]; then
